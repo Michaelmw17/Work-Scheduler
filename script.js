eea74ec011791efc0727ff1f0daf991daa181b22
Michaelmw17 <Michaelmw17@outlook.com>
2020-09-12 23:09:28 +1000

    jquery

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function clock() {
   $("#timeCurrent").html(date);
 }
 setInterval(clock, 1000);
+
+// Text area for schedule
+for (var hour = 9; hour < 18; hour++) {
+  scheduledHours.push(moment({ hour }).format("h a"));
+  $(".container").append(`<div class=' row time' data-time='${hour}'>
+           <!--hour column-->
+               <div class='col-sm col-md-2 hour'>
+                 <p>${moment({ hour }).format("h a")}</p>
+               </div>
+            <!--scheduling column-->
+               <div class='col-sm col-md-9 d-flex '>
+                  <div class='input-group'>
+                    <textarea class="form-control text-area"></textarea>
+                    <div class='input-group-append'>
+                      <button class='save-button d-flex justify-center align-center'>
+                      <i class="far fa-2x fa-save"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>`);
+};
