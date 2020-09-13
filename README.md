dad15cab4c663c53854e538f01e9699b749d6592
Michaelmw17 <Michaelmw17@outlook.com>
2020-09-14 03:10:42 +1000

    jquery

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# w5homework
+# TITLE
+
+<h1>
+Work Day Scheduler
+</h1>
+## Contents
+
+<p>
+  The app is composed of 3 pages, index.html, javascript.js and style.css. It may contain images in the img folder.
+</p>
+
+## User Story
+
+<p>
+  As a type of user, Work Scheduler that functions on the click, with a save btn ford work-data stored with local storage.
+</p>
+
+## Built With
+
+- [VScode](https://code.visualstudio.com/) - The editor of choice.
+- [Terminal](https:///) - What would we do without our bash.
+
+#### Initial interface
+
+<hr>
+
+<img src="./img/w5homework.png">
+
+#### Final Interface
+
+-
+
+### Licence
+
+https://github.com/Michaelmw17/w5homework/blob/master/LICENSE
+
+### Link to the App
+
+<a href=".">https://michaelmw17.github.io/w5homework/</a><hr>
+
+<hr>
+
+## Authors
+
+- **FIRST LAST** -
+  Michael Watt
+
+michaelmw17@outlook.com
+
+## Acknowledgments/Sources
+
+- textContent vs innerHTML/innerText
+
+  - https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+  - https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#Differences_from_innerText
+
+- Local Storage
+
+  - https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+- Timers
+
+  - https://medium.com/@monica1109/scheduling-settimeout-and-setinterval-ca2ee50cd99f
+
+- Accessing array items
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+- Dark Mode With One Line Of Code
+
+https://dev.to/ekaterina_vu/dark-mode-with-one-line-of-code-4lkm
+
+- JQuery
+https://api.jquery.com/attr/
+https://api.jquery.com/closest/#closest-selector
+https://api.jquery.com/jQuery.Callbacks/#stopOnFalse
+https://api.jquery.com/val/#val
+https://api.jquery.com/find/#find-selector
+https://api.jquery.com/each/#each-function
--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Variables
-var availableHours = {};
+var hoursAvailable = {};
 var scheduledHours = [];
 var m = moment();
 var newDay = moment().hour(0);
 var currentTime = m.hour();
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // Add clock to timeCurrent id
 function clock() {
@@ -14,22 +20,108 @@ setInterval(clock, 1000);
 
 // Text area for schedule
 for (var hour = 9; hour < 18; hour++) {
-  scheduledHours.push(moment({ hour }).format("h a"));
-  $(".container").append(`<div class=' row time' data-time='${hour}'>
-           <!--hour column-->
-               <div class='col-sm col-md-2 hour'>
-                 <p>${moment({ hour }).format("h a")}</p>
-               </div>
-            <!--scheduling column-->
-               <div class='col-sm col-md-9 d-flex '>
-                  <div class='input-group'>
-                    <textarea class="form-control text-area"></textarea>
-                    <div class='input-group-append'>
-                      <button class='save-button d-flex justify-center align-center'>
-                      <i class="far fa-2x fa-save"></i>
-                      </button>
-                    </div>
+  scheduledHours.push(moment({ hour }).format("h  a"));
+  $(".container").append(`<div class='row time-block' data-time='${hour}'>
+         <!--hour column-->
+             <div id='col' class='col-sm col-md-2 hour'>
+               <p>${moment({ hour }).format("h  a")}</p>
+             </div>
+          <!--scheduling column-->
+             <div class='col-sm col-md-10 d-flex description'>
+                <div class='input-group'>
+                  <textarea class="form-control text-area"></textarea>
+                  <div class='input-group-append'>
+                    <button class='save-button d-flex justify-center align-center'>
+                      <i class='far fa-save fa-2x save-icon'></i>
+                    </button>
                   </div>
                 </div>
-              </div>`);
-};
+              </div>
+            </div>`);
+}
+
+//Checking time to determine present, past, or future
+$.each($(".time-block"), function (index, value) {
+  let dateHour = $(value).attr("data-time");
+  if (Number(dateHour) === m.hour()) {
+    $(this).find("textarea").addClass("present");
+  } else if (Number(dateHour) < m.hour()) {
+    $(this).find("textarea").addClass("past").attr("disabled", "disabled");
+    $(this).find(".save-button").addClass("disabled").attr("disabled", true);
+  } else {
+    $(this).find("textarea").addClass("future");
+  }
+});
+
+console.log(currentTime);
+
+if (currentTime >= 0 && currentTime < 9) {
+  localStorage.clear();
+}
+
+//Check for local storage to set value to the object and clearing if currentTime is between 12am and 9am
+if (localStorage.getItem("hoursAvailable")) {
+  hoursAvailable = JSON.parse(localStorage.getItem("hoursAvailable"));
+} else {
+  hoursAvailable = {
+    9: {
+      time: "9",
+      value: "",
+    },
+    10: {
+      time: "10",
+      value: "",
+    },
+    11: {
+      time: "11",
+      value: "",
+    },
+    12: {
+      time: "12",
+      value: "",
+    },
+    13: {
+      time: "13",
+      value: "",
+    },
+    14: {
+      time: "14",
+      value: "",
+    },
+    15: {
+      time: "15",
+      value: "",
+    },
+    16: {
+      time: "16",
+      value: "",
+    },
+    17: {
+      time: "17",
+      value: "",
+    },
+  };
+}
+
+// Set value of hoursAvailable to equal the user input for each row
+
+$(".time-block").each(function () {
+  $(this)
+    .find(".text-area")
+    .val(hoursAvailable[$(this).attr("data-time")].value);
+});
+
+//save value to local storage on click
+$(".save-button").on("click", function (event) {
+  event.preventDefault();
+
+  //set hoursAvailable time attribute
+  var timeValue = $(this).closest(".time-block").attr("data-time");
+
+  //set hoursAvailable value attribute
+  var textValue = $(this).closest(".time-block").find(".text-area").val();
+  hoursAvailable[timeValue].value = textValue;
+
+  //save user input in each object to local storage
+  localStorage.setItem("hoursAvailable", JSON.stringify(hoursAvailable));
+});
